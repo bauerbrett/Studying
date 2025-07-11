360a0753986f932247bb4c1e05695cdad9cf57ba
bauerbrett <bauerbrett1@gmail.com>
2025-07-10 21:09:30 -0400

    long palindrome

---
+++ Notes/hashmap/longestpalindrome/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// count letters in string and put in map. Add all evens up add odds but subtrack -1 from them. Add all up and this is longest.
+// if the total length is odd than add one char for a middle character
+func longestPalindrome(p string) int {
+
+	charCount := map[byte]int{}
+	for i := 0; i < len(p); i++ {
+		charCount[p[i]]++
+	}
+	count := 0
+	oddLength := false
+	for _, value := range charCount {
+		if value%2 == 0 {
+			count += value
+		}
+		if value%2 != 0 {
+			count += value - 1
+			oddLength = true
+		}
+	}
+	if oddLength {
+		count++
+	}
+	return count
+
+}
+
+func main() {
+	pali := "bananas"
+	fmt.Println(longestPalindrome(pali))
+
+}
