ae2b68cbcb4d8e75310e789f8ec58ef8ae06df6d
bauerbrett <bauerbrett1@gmail.com>
2025-07-16 21:35:13 -0400

    path exist question

---
+++ Notes/graph/pathexist/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+type Graph struct {
+	vert   int
+	matrix [][]int
+}
+
+func Constructor(v int) *Graph {
+	g := &Graph{vert: v, matrix: make([][]int, v)}
+	return g
+}
+
+func (g *Graph) addEdges(a, b int) {
+	g.matrix[a] = append(g.matrix[a], b)
+	g.matrix[b] = append(g.matrix[b], a)
+}
+
+func pathExist(g *Graph, start, end int) bool {
+	visited := make([]bool, g.vert)
+
+	var dfs func(*Graph, int, int) bool
+	dfs = func(g *Graph, a, b int) bool {
+		visited[a] = true
+		if a == b {
+			return true
+		}
+		path := false
+		for _, neighbor := range g.matrix[a] {
+			if !visited[neighbor] {
+				path = path || dfs(g, neighbor, end)
+				/* same as
+				if dfs(g, neighbor, end) {
+					path = true
+				}
+				*/
+			}
+		}
+		return path
+
+	}
+	return dfs(g, start, end)
+}
+
+func main() {
+	g := Constructor(4)
+	g.addEdges(0, 1)
+	g.addEdges(1, 2)
+	g.addEdges(2, 3)
+	fmt.Println(pathExist(g, 0, 3))
+}
